bf9c5c31c04b34671c6b9405c33fe1a6c2c78154
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-09 17:48:32 +0100

    Added bootstrap and jquery

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title>Placar</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="container">
+<h1>header</h1>
+<hr />
