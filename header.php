1d4b931c8d2b9e554004da6c97052de7f20bb2cb
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-15 01:36:15 +0100

    Scoreboard index

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
-<?php
-
-require_once 'functions.php';
-
-?>
+<?php require_once 'functions.php'; ?>
 <!doctype html>
 <html>
 <head>
-	<title>Placar</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<title>Scoreboard</title>
 	<link rel="stylesheet" href="css/style.css" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/scoreboard.js"></script>
 </head>
 <body>
-<div class="container">
--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php require_once 'header.php'; ?>
-<h1>Hi</h1>
+
 <?php require_once 'footer.php'; ?>
