1b42ea420618f4263f3f05a93be76831498053cd
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-15 02:15:50 +0100

    Styling scoreboard for halloween

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
-<?php require_once 'header.php'; ?>
-
-<?php require_once 'footer.php'; ?>
+<?php require_once 'functions.php'; ?>
+<!doctype html>
+<html>
+<head>
+	<title>Scoreboard</title>
+	<link rel="stylesheet" href="css/style.css" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="js/scoreboard.js"></script>
+</head>
+<body>
+	<div class="scoreboard">
+		<ul>
+			<li>
+				<span class="position">1</span>
+				<span class="name">TeamA</span>
+				<span class="points">128</span>
+			</li>
+			<li>
+				<span class="position">2</span>
+				<span class="name">TeamA</span>
+				<span class="points">128</span>
+			</li>
+			<li>
+				<span class="position">3</span>
+				<span class="name">TeamA</span>
+				<span class="points">128</span>
+			</li>
+			<li>
+				<span class="position">4</span>
+				<span class="name">TeamA</span>
+				<span class="points">128</span>
+			</li>
+			<li>
+				<span class="position">5</span>
+				<span class="name">TeamA</span>
+				<span class="points">128</span>
+			</li>
+		</ul>
+	</div>
+</body>
+</html>
