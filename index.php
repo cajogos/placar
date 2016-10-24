91902c36a69bc0d21207f57702dd763aa79fca0b
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-24 23:42:31 +0100

    target blank lol

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once 'functions.php';
 		<hr />
 		<p>Choose from one of the options below:</p>
 		<p class="alert alert-warning">Are you part of a team? Then get your coding and physical task by clicking the <strong>Get Task</strong> button.</p>
-		<a href="tasks/get.php" class="btn btn-lg btn-success">Get Task</a>
+		<a href="tasks/get.php" target="_blank" class="btn btn-lg btn-success">Get Task</a>
 		<a href="scoreboard" target="_blank" class="btn btn-lg btn-warning">Scoreboard</a>
 	</div>
 </body>
