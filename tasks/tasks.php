87273aff47e7bc14f8e8444e142c874d4fb70745
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-28 00:05:10 +0100

    Added tasks 4, 5 and 6

--- tasks/tasks.php
+++ tasks/tasks.php
@@ -27,25 +27,11 @@ $TASKS[] = array(
 	);
 
 $TASKS[] = array(
-	'id' => 'E',
-	'view' => 'task5.html'
+	'id' => 'javascript',
+	'view' => '005_javascript.html'
 	);
 
 $TASKS[] = array(
-	'id' => 'F',
-	'view' => 'task6.html'
+	'id' => 'python',
+	'view' => '006_python.html'
 	);
-
-/*
-
-Spooky HTML task
-
-For this task use JSFiddle to create a web page in HTML following this task
-
-Add the body tag to the html section (1 points)
-Create a header that states your teams name (1 points)
-Add an image that of a spooky halloween creature (2 points)
-Add a short paragraph with your team names and your ages (1 points)
-Change the background colour of the page (2 points)
-Change the colour of the font so that you can read the text clearly (2 points)
-Center everything in the page (3 points)*/
